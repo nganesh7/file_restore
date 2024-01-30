fe8d96326c3b40c84b4427bd810e751d087f3690
nganesh7 <nandanganesh@gmail.com>
2024-01-30 23:52:31 +0000

    Add files via upload

--- ImageRestore.py
+++ ImageRestore.py
@@ -11,26 +11,30 @@ FILE_SIGNATURES = {
     b'\x47\x49\x46\x38\x37\x61': 'gif',
     b'\x47\x49\x46\x38\x39\x61': 'gif',
     b'\x42\x4d': 'bmp',
+    b'\x01\xda': 'rgb',
+    b'\x4d\x4d\x00\x2a': 'tif',
+    b'\x49\x49\x2a\x00': 'tif',
 }
 
 
 def restore_files(folder_path):
-    # Create a directory to store restored files
+    # Creating a directory to store restored files
     restored_dir = os.path.join(folder_path, "Restored_Files")
+    unidentified_dir = os.path.join(folder_path, "Unidentified_Files")
     os.makedirs(restored_dir, exist_ok=True)
-    
+    os.makedirs(unidentified_dir, exist_ok=True)
     
     try:
-        # Iterate over files in the given folder
+        # Iterating over files in the given folder
         for filename in os.listdir(folder_path):
             filepath = os.path.join(folder_path, filename)
             if os.path.isfile(filepath):
                 with open(filepath, 'rb') as f:
-                    # Read the first few bytes to identify the file signature
+                    # Reading the first few bytes to identify the file signature
                     file_signature = f.read(8)
                     for signature, extension in FILE_SIGNATURES.items():
                         if file_signature.startswith(signature):
-                            # Move the file to corresponding folder based on image type
+                            # Moving the file to corresponding folder based on image type
                             image_folder = os.path.join(restored_dir, extension)
                             os.makedirs(image_folder, exist_ok=True)
                             new_filename = f"{filename}.{extension}"  # Adding the extension back to filename
@@ -39,19 +43,22 @@ def restore_files(folder_path):
                             break
                     else:
                         print(f"Could not identify type for file: {filename}")
+                        new_filepath = os.path.join(unidentified_dir, filename)
+                        shutil.copy(filepath, new_filepath)
+                        print(f"Copied to Unidentified folder: {filename}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 def main():
-    # Check if folder path is provided as an argument
+    # Checking if folder path is provided as an argument
     if len(sys.argv) != 2:
         print("Error! Missing argument: ./FileRestore.py <folder_path>")
         sys.exit(1)
     
     folder_path = sys.argv[1]
     
-    # Check if the provided path is a directory
+    # Checking if the provided path is a directory
     if not os.path.isdir(folder_path):
         print("Error: Provided path is not a directory.")
         sys.exit(1)
